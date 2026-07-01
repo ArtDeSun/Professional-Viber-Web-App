9d749072ed35379e50fde95dcf4dd10f7650b3cc
Steven Sun <jiasun0916@gmail.com>
2026-07-01 16:48:19 -0400

    Animate buttons after session has been loaded.

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -26,11 +26,12 @@ export default function Home() {
   const isLoggedIn = Boolean(session?.user);
 
   useEffect(() => {
+    if (!initialAuthChecked) return;
     const timer = setTimeout(() => {
       setVisible(true);
     }, 100);
     return () => clearTimeout(timer);
-  }, []);
+  }, [initialAuthChecked]);
   /* return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
